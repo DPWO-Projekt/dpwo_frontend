0ca7e5b457164c208dff477b7f7c60fffe6f9548
Paweł Cegielski <skok2022@gmail.com>
2025-03-25 21:09:43 +0100

    Modal window with basic bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {Route, Routes} from "react-router";
 import Catalog from "./components/Catalog/Catalog";
 import Home from "./components/Home/Home";
+import AddDataSet from './components/AddDataSet/AddDataSet';
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/catalog" element={<Catalog/>}/>
+                <Route path="/adddataset" element={<AddDataSet/>}/>
             </Routes>
         </div>
     );
